4cac09067a10137811dbb6485555d733f535e8c1
Hanna George <hannageorge018@gmail.com>
2025-04-02 18:21:01 -0400

    Created models for account, login and customers

--- rivanna/rivannabank/admin.py
+++ rivanna/rivannabank/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Customer, Account, Transaction, FundTransfer,Login
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Account)
+admin.site.register(Transaction)
+admin.site.register(FundTransfer)
+admin.site.register(Login)
